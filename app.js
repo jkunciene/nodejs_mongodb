85573c23e2b3a5c00a31d471e4f42294045137d6
jkunciene <j.kunciene@gmail.com>
2022-12-29 15:09:29 +0200

    login

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 const port = process.env.PORT || 4000;
 
@@ -7,8 +9,13 @@ connectDB();
 
 const app = express();
 
+app.use(cors());
+app.options('*', cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/users', require('./routes/userRoutes'));
+
+app.use(errorHandler);
 
 app.listen(port, ()=> console.log(`Server is running on port ${port}`));
--- controllers/userController.js
+++ controllers/userController.js
@@ -43,13 +43,36 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 })
 
+// @desc Login a user
+// @route POST /api/users/login
+// @access PUBLIC
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body
+  
+    const user = await User.findOne({ email })
+  
+    if (user && (await bcrypt.compare(password, user.password))) {
+      res.json({
+        _id: user.id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id),
+        role: user.role
+      })
+    } else {
+      res.status(400)
+      throw new Error('Invalid credentials')
+    }
+  })
+  
 // Generate JWT
 const generateToken = id => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
       expiresIn: '30d'
     })
   }
-  
+
 module.exports = {
     registerUser,
+    loginUser
 }

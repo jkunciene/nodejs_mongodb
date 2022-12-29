55fd80983bdc5a7fb6936eadd190295390ca525a
jkunciene <j.kunciene@gmail.com>
2022-12-29 17:16:08 +0200

    update, delete

--- controllers/adController.js
+++ controllers/adController.js
@@ -27,7 +27,66 @@ const setAd = asyncHandler(async (req, res) => {
     res.status(200).json(ad)
   })
 
+  // @desc Update goal
+// @route PUT /api/goals/:id
+// @access PRIVATE
+const updateAd = asyncHandler(async (req, res) => {
+  const ad = await Ad.findById(req.params.id)
+
+  if (!ad) {
+    res.status(400)
+    throw new Error('Ad not found')
+  }
+
+  // check for user
+  if (!req.user) {
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  // make sure the logged in user matches the goal user
+  if (ad.user.toString() !== req.user.id) {
+    res.status(401)
+    throw new Error('User not authorized')
+  }
+
+  const updateAd = await Ad.findByIdAndUpdate(req.params.id, req.body, {
+    new: true
+  })
+  res.status(200).json(updateAd)
+})
+
+// @desc Delete goal
+// @route DELETE /api/goals/:id
+// @access PRIVATE
+const deleteAd = asyncHandler(async (req, res) => {
+  const ad = await Ad.findById(req.params.id)
+
+  if (!ad) {
+    res.status(400)
+    throw new Error('Ad not found')
+  }
+
+  // check for user
+  if (!req.user) {
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  // make sure the logged in user matches the goal user
+  if (ad.user.toString() !== req.user.id) {
+    res.status(401)
+    throw new Error('User not authorized')
+  }
+
+  await ad.remove()
+
+  res.status(200).json({ id: req.params.id })
+})
+
   module.exports = {
     getAds,
     setAd,
+    updateAd,
+    deleteAd
   }
--- routes/adRoutes.js
+++ routes/adRoutes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const {
-    setAd, getAds,
+    setAd, getAds, updateAd, deleteAd
 } = require('../controllers/adController');
 
 const { protect } = require('../middleware/authMiddleware');
 
 router.route('/').get(protect, getAds).post(protect, setAd);
+router.route('/:id').put(protect, updateAd).delete(protect, deleteAd);
 
 module.exports = router;
